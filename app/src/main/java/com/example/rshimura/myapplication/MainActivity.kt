e41d5bcf35a6ebad7c127ec0a0d608996d5aacab
Ryo Shimura <rshimura@bi.a.u-tokyo.ac.jp>
2017-07-09 08:58:05 +0900

    0709 0909

--- app/src/main/java/com/example/rshimura/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/rshimura/myapplication/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.rshimura.myapplication
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.VectorEnabledTintResources
+import android.widget.EditText
 import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
 
     private var mTextMessage: TextView? = null
+    private var testedit : EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-                val navigation = findViewById(R.id.navigation) as BottomNavigationView
+        val navigation = findViewById(R.id.navigation) as BottomNavigationView
         //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         navigation.setOnNavigationItemSelectedListener { item ->
--- app/src/main/java/com/example/rshimura/myapplication/WriteFragment.kt
+++ app/src/main/java/com/example/rshimura/myapplication/WriteFragment.kt
@@ -5,11 +5,16 @@ import android.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
 import de.timroes.android.listview.EnhancedListView
 import java.util.*
+import android.view.inputmethod.InputMethodManager.RESULT_UNCHANGED_SHOWN
+import android.view.KeyEvent.KEYCODE_ENTER
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
+
 
 /**
  * Created by rshimura on 2017/07/08.
@@ -23,13 +28,25 @@ public class WriteFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val v : View = inflater.inflate(R.layout.write_fragment, container, false)
+        val v: View = inflater.inflate(R.layout.write_fragment, container, false)
 
+        val itemList: MutableList<String> = mutableListOf()
+        val listView: EnhancedListView      = v.findViewById(R.id.todolist) as EnhancedListView
+        val adapter : ArrayAdapter<String>  = ArrayAdapter<String>(v.context, android.R.layout.simple_list_item_1, itemList)
+
+        defToDoListView(v, itemList, listView, adapter)
+        defListItemIO  (v, itemList, listView, adapter)
+
+        return v
+    }
+
+    private fun defToDoListView(v: View, itemList: MutableList<String>,
+                                listView: EnhancedListView, adapter: ArrayAdapter<String>): Unit {
         // for todo list view
-        var itemList : LinkedList<String>    = emptyList<String>() as LinkedList<String>
-        val listView : EnhancedListView      = v.findViewById<EnhancedListView>(R.id.todolist)
-        val adapter  : ArrayAdapter<String>  = ArrayAdapter<String>(v.context, R.layout.todo_list, itemList)
         listView.setAdapter(adapter)
+        listView.setRequireTouchBeforeDismiss(false)
+        listView.setUndoHideDelay(3000)
+
         listView.setDismissCallback { listView : EnhancedListView, pos : Int ->
             val item = adapter.getItem(pos)
             itemList.removeAt(pos)
@@ -41,12 +58,55 @@ public class WriteFragment : Fragment() {
                 }
             }
         }
-        //
+        listView.enableSwipeToDismiss();
+    }
 
-        // input text and add list
-        val inputText : EditText = v.findViewById(R.id.inputText)
-        val goBtn  : Button      = v.findViewById(R.id.go_button)
+    private fun defListItemIO(v : View, itemList: MutableList<String>,
+                              listView: EnhancedListView, adapter: ArrayAdapter<String>): Unit {
+// input text and add list
+        val inputText: EditText = v.findViewById(R.id.inputText) as EditText
+        val goBtn    : Button   = v.findViewById(R.id.go_btn)    as Button
+        var clickedPosition : Int? = null
+        // def btn action
+        val pushToDo = {
 
-        return v
+            when (goBtn.text) {
+                "Go" -> {
+                    var inputStr = inputText.text.toString()
+                    itemList.add(inputStr)
+                    adapter.notifyDataSetChanged()
+                    inputText.setText("")
+                }
+                "Re" -> {
+                    var inputStr = inputText.text.toString()
+                    itemList.set(clickedPosition!!, inputStr)
+                    adapter.notifyDataSetChanged()
+                    inputText.setText("")
+                    goBtn.setText("Go")
+                }
+            }
+        }
+
+        // edit list
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val item    = parent.getItemAtPosition(id.toInt())
+            val editStr = item.toString()
+            clickedPosition = position
+            inputText.setText(editStr)
+            goBtn.setText("Re")
+        }
+        goBtn.setOnClickListener(View.OnClickListener { pushToDo() })
+        // when enter is clicked, push todo
+        inputText.setOnKeyListener(object : View.OnKeyListener {
+
+            override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
+
+                if (event.getAction() === KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    pushToDo()
+                    return true
+                }
+                return false
+            }
+        })
     }
 }
